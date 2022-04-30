a8f026fc07e19c892565d7179502fdaadd6f3dcb
Сергей Иваница <sergei-ivanitsa@yandex.ru>
2022-05-01 08:15:05 +1000

    23

--- settings.py
+++ settings.py
@@ -1,5 +1,10 @@
-from homework import PRACTICUM_TOKEN
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
